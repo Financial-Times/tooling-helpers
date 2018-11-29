7e9e9251980fc7ed181ef679a168165a3758d27e
Keran Braich <keran.braich@ft.com>
2018-11-29 17:30:14 +0000

    Include octokit when requiring files

--- github/index.js
+++ github/index.js
@@ -1,9 +1,4 @@
-require('dotenv').config()
-const createProject = require('./lib/create-project');
-const createProjectColumn = require('./lib/create-project-column');
-const createNoteCard = require('./lib/create-note-card');
-const createPullRequest = require('./lib/create-pull-request');
-const createPullRequestCard = require('./lib/create-pull-request-card');
+require('dotenv').config();
 
 const octokit = require('@octokit/rest')({
 	headers: {
@@ -17,67 +12,16 @@ octokit.authenticate({
 	token: process.env.GITHUB_KEY
 });
 
-(async () => {
-	// Create the project which returns an object with a project ID
+const createProject = require('./lib/create-project')(octokit);
+const createProjectColumn = require('./lib/create-project-column')(octokit);
+const createNoteCard = require('./lib/create-note-card')(octokit);
+const createPullRequest = require('./lib/create-pull-request')(octokit);
+const createPullRequestCard = require('./lib/create-pull-request-card')(octokit);
 
-	const project = await createProject.createProject(octokit, {
-		org: 'financial-times-sandbox',
-		name: 'Sample Project'
-	});
-
-	const projectId = project.data.id;
-
-	// Create 'To do', 'In progress' and 'Done' columns which return an object with a column IDs
-
-	const toDoColumn = await createProjectColumn.createProjectColumn(octokit, {
-		project_id: projectId,
-		name: 'To do'
-	});
-
-	const toDoColumnId = toDoColumn.data.id;
-
-	const inProgressColumn = await createProjectColumn.createProjectColumn(octokit, {
-		project_id: projectId,
-		name: 'In progress'
-	});
-
-	const inProgressColumnId = inProgressColumn.data.id;
-
-	const doneColumn = await createProjectColumn.createProjectColumn(octokit, {
-		project_id: projectId,
-		name: 'Done'
-	});
-
-	const doneColumnId = doneColumn.data.id;
-
-	// Create a card with a note which returns an object with a card ID
-
-	const noteCard = await createNoteCard.createNoteCard(octokit, {
-		column_id: toDoColumnId,
-		note: 'Sample note'
-	});
-
-	const cardId = noteCard.data.id;
-
-	// Create a PR which returns an object with a PR IDs
-
-	const pullRequest = await createPullRequest.createPullRequest(octokit, {
-		owner: 'financial-times-sandbox',
-		repo: 'Western-Storm',
-		title: 'Sample pull request',
-		head: 'kb/sample-pull-request',
-		base: 'master',
-		body: 'Pull request body'
-	});
-
-	const pullRequestId = pullRequest.data.id;
-
-	// Create a card with a PR
-
-	await createPullRequestCard.createPullRequestCard(octokit, {
-		column_id: toDoColumnId,
-		content_id: pullRequestId,
-		content_type: 'PullRequest'
-	});
-
-})();
+module.exports = {
+	createProject,
+	createProjectColumn,
+	createNoteCard,
+	createPullRequest,
+	createPullRequestCard,
+};
