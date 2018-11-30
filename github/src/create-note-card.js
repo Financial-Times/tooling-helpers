3905aa473d1a109d1038710346950c5b9c9bb61b
Keran Braich <keran.braich@ft.com>
2018-11-30 15:42:37 +0000

    Add GitHub API documentation

--- github/src/create-note-card.js
+++ github/src/create-note-card.js
@@ -1,6 +1,11 @@
 module.exports = (octokit) => {
 
   return async ({ column_id, note }) => {
+    /**
+     * Create a GitHub project card
+     *
+     * @see https://octokit.github.io/rest.js/#api-Projects-createCard
+     */
     const result = await octokit.projects.createCard({ column_id, note });
 
     return result.data;
--- github/src/create-project.js
+++ github/src/create-project.js
@@ -1,6 +1,11 @@
 module.exports = (octokit) => {
 
 	return async ({ org, name }) => {
+		/**
+     * Create a GitHub organisation project
+     *
+     * @see https://octokit.github.io/rest.js/#api-Projects-createForOrg
+     */
 		const result = await octokit.projects.createForOrg({ org, name });
 
 		return result.data;
