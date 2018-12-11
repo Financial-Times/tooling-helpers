aa98c518f14b78b36a78c8e0da73fbd9bb0ca705
Keran Braich <keran.braich@ft.com>
2018-12-11 11:22:09 +0000

    Fix comment indententation (hopefully)

--- github/src/create-card.js
+++ github/src/create-card.js
@@ -2,10 +2,10 @@ module.exports = (octokit) => {
 
 	return async ({ column_id, content_id, content_type }) => {
 		/**
-     * Create a GitHub project card
-     *
-     * @see https://octokit.github.io/rest.js/#api-Projects-createCard
-     */
+		 * Create a GitHub project card
+		 *
+		 * @see https://octokit.github.io/rest.js/#api-Projects-createCard
+		 */
 		const result = await octokit.projects.createCard({ column_id, content_id, content_type });
 
 		return result.data;
